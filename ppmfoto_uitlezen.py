ac1d4f95182c285c26b9570be64337bad928ee27
WassimLazaar <w.lazaar@student.avans.nl>
2025-05-10 14:13:00 +0200

    ppmfoto_uitlezen python file toegevoegd

---
+++ ppmfoto_uitlezen.py
@@ -0,0 +1,14 @@
+import cv2
+
+# Specificeer het pad naar het PPM-bestand
+image_path = r'C:\Users\Rachid\Documents\GTSRB_Dataset\GTSRB_Final_Training_Images\GTSRB\Final_Training\Images\00000\00005_00017.ppm'
+
+# Lees de afbeelding in
+img = cv2.imread(image_path)
+
+# Toon de afbeelding in een nieuw venster
+cv2.imshow('Verkeersbord', img)
+
+# Wacht op een toetsdruk om het venster te sluiten
+cv2.waitKey(0)
+cv2.destroyAllWindows()
